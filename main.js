70c82bd8f30d6410f7a50ac543fa029400b5ae84
cogcamp <codeofgenius.camp@gmail.com>
2024-12-24 06:01:07 +0000

    問題用にプログラム修正

--- main.js
+++ main.js
@@ -11,22 +11,20 @@ mainScene.create = function() {
   this.createPlane();
   
   // スペースキーでビーム発射
-  this.input.keyboard.on('keydown-SPACE', function(event) {
-    this.createBeam();
-  }, this);
+
   
   // ゲームオーバーを表す変数。初期値は「false」
   this.isGameOver = false;
   
   // ビームグループの作成
-  this.createBeamGroup();
-  
+
+
   // 敵エイリアンの作成
-  this.createAlian();
-  
+
+
   // 敵レーザーグループの作成
-  this.createLaserGroup();
-  
+
+
 };
 
 mainScene.update = function() {
@@ -53,14 +51,9 @@ mainScene.update = function() {
   }
   
   // 敵となるエイリアンが画面上部を左右に移動する
-  if (this.alian.x < 100) {
-    this.alian.dx = -this.alian.dx;
-  }
-  if (this.alian.x > 700) {
-    this.alian.dx = -this.alian.dx;
-  }
-  this.alian.x += this.alian.dx;
-  
+
+
+
 };
 
 mainScene.createBackground = function() {
@@ -79,89 +72,58 @@ mainScene.createPlane = function() {
 
 mainScene.createBeamGroup = function() {
   // ビームグループを作成する
-  this.beamGroup = this.physics.add.group();
+
+
+
 };
 
 mainScene.createBeam = function() {
   // 飛行機が発射するビームを作成する
-  var x = this.plane.body.center.x;
-  var y = this.plane.body.center.y;
-  
-  var beam = this.beamGroup.create(x, y, 'beam');
-  beam.setDisplaySize(30, 30);
-  var speed = 700;
-  beam.setVelocityY(-speed);
-  beam.body.setAllowGravity(false);
+
+
+
 };
 
 mainScene.createAlian = function() {
   // 敵となるエイリアンを作成する
-  this.alian = this.physics.add.image(400, 80, 'alian');
-  this.alian.setDisplaySize(150, 150);
-  this.alian.body.setAllowGravity(false);
-  this.alian.hp = 10;
-  this.alian.dx = 5;
-  // 文字の表示
-  this.bossText = this.add.text(650, 30, "HP:"+this.alian.hp , {
-      font: '40px Open Sans',
-      fill: '#ff0000'
-  });      
-  this.physics.add.overlap(this.alian, this.beamGroup, this.hitAlian, null, this);
-  
+
+
+
 };
 
 mainScene.createLaserGroup = function() {
   // レーザーグループを作成する
-  this.laserGroup = this.physics.add.group();
-  
-  this.physics.add.overlap(this.plane, this.laserGroup, this.hitPlane, null, this);  
-  this.physics.add.overlap(this.laserGroup, this.beamGroup, this.hitLaser, null, this);  
-  
-  this.laserTimer = this.time.addEvent({
-    delay : 500,
-    callback : this.createLaser,
-    loop : true,
-    callbackScope : this
-  });  
+
+
+
 };
 
 mainScene.createLaser = function() {
   // エイリアンの位置にレーザーを作成する
-  var x = this.alian.body.center.x;
-  var y = this.alian.body.center.y;
-  
-  var laser = this.laserGroup.create(x, y, 'laser');
-  laser.setDisplaySize(70, 70);
-  laser.setAngle(180);
-  laser.body.setAllowGravity(false);
-  laser.setVelocityY(300);
+
+
+
 };
 
 mainScene.hitPlane = function(plane, laser) {
   // 飛行機にレーザーが衝突したのでゲームオーバー
-  this.gameOver();
-  
+
+
+
 };
 
 mainScene.hitLaser = function(laser, beam) {
   // レーザーとビームが衝突したので両方を削除
-  laser.destroy();
-  beam.destroy();
-  
+
+
+
 };
 
 mainScene.hitAlian = function(alian, beam) {
   // エイリアンにビームが衝突したときの処理
-  beam.destroy();
-  alian.hp--;
-  var text = "HP:" + alian.hp;
-  
-  if (alian.hp < 0) {
-    this.gameClear();
-  } else {
-    this.bossText.setText(text);
-  }
-  
+
+
+
 };
 
 mainScene.gameOver = function() {
